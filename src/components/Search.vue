c9deee3a7b8b96969dbc0e2214761dc0333ea4df
Kos Max <maxie7@protonmail.com>
2017-09-11 22:07:28 +0200

    Initial Commit for searcher)

---
+++ src/components/Search.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="Search">
+        <input type="text" class="Search__input" v-model="query">
+        <button class="Search__button">Search</button>
+    </div>
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+          query: ''
+        }
+      }
+    }  
+</script>
